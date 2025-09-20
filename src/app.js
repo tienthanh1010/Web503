216d61a5d36d693ab8be928b3b6b369815a0df7e
Vu Tien Thanh <your_email@example.com>
2025-09-20 17:40:16 +0700

    Bai2

--- src/app.js
+++ src/app.js
@@ -1,7 +1,20 @@
 import express from "express";
 
 const app = express();
+app.get("/api/posts/greet", (req, res) => {  
+  const name = req.query.name || "World";  
+  res.send(`Hello, ${name}! Buổi tối vui vẻ `);  
+});
 
+app.get("/api/posts/sum", (req, res) => {
+  const a = parseFloat(req.query.a);
+  const b = parseFloat(req.query.b);
+  if (isNaN(a) || isNaN(b)) {
+    return res.status(400).send("Invalid numbers provided.");
+  }
+  const sum = a + b;
+  res.send(`The sum of ${a} and ${b} is ${sum}.`);
+});
 app.listen(3000, () => {
   console.log(`Server is running on port http://localhost:3000`);
 });
